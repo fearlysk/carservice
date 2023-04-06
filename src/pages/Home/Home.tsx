f3dcf13ae90e0c14bf483eded34db9f32c5f280c
fearlysk <sqleye1@mail.ru>
2023-04-06 23:54:26 +0300

    add homepage main block

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,27 @@
 import HomeBackground from "../../assets/Home/images/home_background.png";
+import Car from "../../assets/Home/images/home_car.png";
 import Header from "../../components/Header/Header";
 import styles from "./Home.module.scss";
+import Star from "../../components/UI/Logos/Star/Star";
 
 const Home = () => {
     return (
         <div className={styles.wrapper} style={{background: `url(${HomeBackground})`}}>
             <div className={styles.container}>
                <Header />
+               <div className={styles.main}>
+                    <div className={styles.main__item}>
+                        <p className={styles.main__itemHeadline}>ПРОФЕССИОНАЛЬНЫЙ РЕМОНТ И ОБСЛУЖИВАНИЕ АВТОМОБИЛЕЙ</p>
+                        <p className={styles.main__itemDescription}>Узнайте стоимость ремонта вашего автомобиля,пройдя тест из 3-х вопросов</p>
+                        <div className={styles.main__itemStar}><Star /></div>
+                        <button className={styles.main__itemButton}><p className={styles.main__itemButtonText}>УЗНАТЬ СТОИМОСТЬ РЕМОНТА</p></button>
+                    </div>
+                    <div className={styles.main__item}>
+                        <div className={styles.main__itemCar}>
+                            <img src={Car} alt='No img found'/>
+                        </div>
+                    </div>
+               </div>
             </div>
         </div>
     )
